8878f7f6289749ee60edb9d7848645f760e9e01b
Mamanibhavana <161690487+Mamanibhavana@users.noreply.github.com>
2024-03-05 20:55:56 +0530

    Add files via upload

---
+++ code soft rock.py
@@ -0,0 +1,31 @@
+import random
+
+select = ("rock", "paper", "scissors")
+running = True
+
+while running:
+
+    player = None
+    computer = random.choice(select)
+
+    while player not in select:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You lose!")
+
+    if not input("Play again? (y/n): ").lower() == "y":
+        running = False
+
+print("Thanks for playing!")
